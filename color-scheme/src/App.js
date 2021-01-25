994fe8b16fe221dd406f4aeec957f849307a9865
sparekh315 <saniaparekh315@gmail.com>
2021-01-24 19:37:22 -0600

    added chroma js, and set up shade generator for hex rgb and rgba"

--- color-scheme/src/App.js
+++ color-scheme/src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import ColorPalette from './Components/ColorPalette';
 import defaultPalettes from './Components/defaultPalettes';
 import './App.css';
+import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
   render() {
-
+      console.log(generatePalette(defaultPalettes[4]));
     return(
       <div>
         <h1>Color Scheme App</h1>
